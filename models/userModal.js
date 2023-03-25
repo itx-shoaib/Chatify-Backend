4e0e9611382249e75702f8ebeace8f18c2d3f3a0
itx-shoaib <shoaibjamil43@gmail.com>
2023-03-26 00:35:35 +0500

    Correcting the type in model

--- models/userModal.js
+++ models/userModal.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var userSchema = mongoose.Schema({
     // This is the basic model schema of user
     username: { type: String, unique: true },
-    password: Strinng,
+    password: String,
 }, { timestamps: true });
 
 const userModal = mongoose.model('users', userSchema)
