00601838030657e1fc0a40406bec3e767fe3d2db
itx-shoaib <shoaibjamil43@gmail.com>
2023-03-26 00:31:42 +0500

    Making route

---
+++ routes/userRoute.js
@@ -0,0 +1,11 @@
+const express = require('express'); //import express
+
+// 1.
+const router = express.Router();
+// 2.
+const UserController = require('../controllers/userRoute');
+// 3.
+router.post('/createUser', UserController.createUser);
+
+// 4. 
+module.exports = router; // export to use in server.js
