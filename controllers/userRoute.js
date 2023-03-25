0ab5d97f64112f722eaa0b406f49896f288a9bfc
itx-shoaib <shoaibjamil43@gmail.com>
2023-03-26 00:49:19 +0500

    making jsonwebtoken

--- controllers/userRoute.js
+++ controllers/userRoute.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv").config()
+var jwt = require('jsonwebtoken');
 const userModal = require("../models/userModal");
+const secret = process.env.MONGO_URI
 let router = express.Router();
 
 const createUser = async (req, res) => {
     try {
         const { username, password } = req.body;
-        const user = new userModal({ username, password });
-        await user.save();
-        res.status(200).json({ message: 'User created successfully' });
+        const user = await userModal.create({ username, password });
+        jwt.sign({ userid: user._id }, secret, (err, token) => {
+            if (err) throw err
+            res.cookie("token", token).status(200).json({ message: 'User created successfully' });
+        })
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
